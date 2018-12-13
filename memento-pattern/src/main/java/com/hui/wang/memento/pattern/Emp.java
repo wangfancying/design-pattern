1010aaad7da68b5b95fb6d7a389a7a2f67cef959
hui.wang09 <hui.wang09@bianlifeng.com>
2018-12-13 19:47:36 +0800

    备忘录模式

---
+++ memento-pattern/src/main/java/com/hui/wang/memento/pattern/Emp.java
@@ -0,0 +1,45 @@
+package com.hui.wang.memento.pattern;
+
+/**
+ * 发起者角色
+ *
+ * @author hui.wang09
+ * @since 13 December 2018
+ */
+public class Emp {
+
+	private String name;
+	private String age;
+
+
+	/**
+	 * 进行备份操作，并返回一个备忘录对象
+	 */
+	public EmpMemento memento() {
+		return new EmpMemento(this);
+	}
+
+	/**
+	 * 进行数据恢复，恢复成备忘录中对象的值
+	 */
+	public void recovery(EmpMemento empMemento) {
+		this.name = empMemento.getName();
+		this.age = empMemento.getAge();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+}
---
+++ memento-pattern/src/main/java/com/hui/wang/memento/pattern/EmpMemento.java
@@ -0,0 +1,34 @@
+package com.hui.wang.memento.pattern;
+
+/**
+ * 备忘录角色
+ *
+ * @author hui.wang09
+ * @since 13 December 2018
+ */
+public class EmpMemento {
+
+	private String name;
+	private String age;
+
+	public EmpMemento(Emp emp) {
+		this.name = emp.getName();
+		this.age = emp.getAge();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+}
